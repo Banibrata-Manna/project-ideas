e8dfa587304efc8dfbcb3a901b4e974c600200ba
Banibrata Santanu Manna <banibrata.manna@hotwaxsystems.com>
2025-10-16 12:05:05 +0530

    Added Average Product Counts in Hard and Directed Counts

--- new-inventory-count/entities/metrics/metrics.sql
+++ new-inventory-count/entities/metrics/metrics.sql
@@ -20,3 +20,30 @@ GROUP BY ici.inventory_count_import_id
 ORDER BY distinct_product_count DESC LIMIT 1;
 
 -- It has been observed on one of our client which uses inventory count creates hard count with average 1000 of products.
+
+
+-- Average Number of Products counted in Directed Count
+
+SELECT AVG(distinct_product_count) FROM (
+    SELECT
+        ici.inventory_count_import_id AS import_id,
+        COUNT(DISTINCT icii.product_id) AS distinct_product_count
+    FROM inventory_count_import ici
+    JOIN inventory_count_import_item icii
+    ON icii.inventory_count_import_id = ici.inventory_count_import_id
+    WHERE ici.count_type_enum_id != 'HARD_COUNT' OR ici.count_type_enum_id is NULL
+    GROUP BY ici.inventory_count_import_id
+) as C;
+
+-- Average Number of Products counted in Hard Count
+
+SELECT AVG(distinct_product_count) FROM (
+    SELECT
+        ici.inventory_count_import_id AS import_id,
+        COUNT(DISTINCT icii.product_id) AS distinct_product_count
+    FROM inventory_count_import ici
+             JOIN inventory_count_import_item icii
+                  ON icii.inventory_count_import_id = ici.inventory_count_import_id
+    WHERE ici.count_type_enum_id = 'HARD_COUNT'
+    GROUP BY ici.inventory_count_import_id
+) as C;
